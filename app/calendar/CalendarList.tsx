8ead2a37ace96ae31e15f44ddf0d63f55f6d2673
Min ho Kim <minho42@gmail.com>
2024-01-19 01:04:26 +1100

    what

--- app/calendar/CalendarList.tsx
+++ app/calendar/CalendarList.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import FullCalendar, { Calendar } from "@fullcalendar/react"
+import FullCalendar from "@fullcalendar/react"
 import dayGridPlugin from "@fullcalendar/daygrid"
 import interactionPlugin from "@fullcalendar/interaction"
 import { useState, useEffect, useRef, useContext, useCallback } from "react"
